454d55a689edb4f075ec280602aa477553435a30
Alexander Tarancon <ataranco@umail.iu.edu>
2019-12-10 14:03:17 -0500

    creating user forms for login,registration,updateuserForm

---
+++ puppycompanyblog/users/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField , PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import Filefield,FileAllowed
+
+from flask_login import current_user
+from puppycompanyblog.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = StringField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired()])
+    username = StringField('UserName',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),EqualTo("pass_conf",message="Password must match")])
+    pass_conf=PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+    #validate for email 
+    def validate_email(self,field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError("Your Email has been already register")
+
+    def validate_username(self,field):
+         if User.query.filter_by(username = field.data).first():
+            raise ValidationError("Your username has been already register")
+        
+class UpdateUserForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    username=StringField("Username",validators=[DataRequired()])
+    picture = Filefield("Update profile picture" , Validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField("Update")
+
+    #validate for email 
+    def validate_email(self,field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError("Your Email has been already register")
+
+    def validate_username(self,field):
+         if User.query.filter_by(username = field.data).first():
+            raise ValidationError("Your username has been already register")
+     
